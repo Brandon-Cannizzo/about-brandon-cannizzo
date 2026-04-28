7015dba3c0ede5d5734910bbce13e2b8b50f2214
Brandon Cannizzo <brandoncaniz4@gmail.com>
2026-04-28 18:44:47 +0000

    Removed pages folder

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-
 main_page = st.Page("1_main_page.py", title="About Me")
 contact_form = st.Page("2_contact_form.py", title="Contact Form")
 pg = st.navigation([main_page, contact_form], position="top")
